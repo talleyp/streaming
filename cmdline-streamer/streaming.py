6d75e0d209d6a9131fb992face46c32d7f5d31aa
Talley <talleyp93@gmail.com>
2019-04-12 16:48:47 -0400

    added user input to exit process when iterating through folder

--- cmdline-streamer/streaming.py
+++ cmdline-streamer/streaming.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import sys
 import glob
 import os
 import argparse
@@ -36,7 +37,7 @@ def get_files(f, args):
     return sorted_list
 
 
-def stream_file(file, args, streamkey):
+def stream_file(file, args, streamkey, iterating=False):
     """
     Create process to stream given file and streamkey
     inpt:
@@ -78,6 +79,10 @@ def stream_file(file, args, streamkey):
             pass
         p.wait()
         time.sleep(1)  # let stream end so there isn't a conflict
+        if iterating:
+            inpt = input('Continue to next file?[y/n] ')
+            if 'N' in inpt.upper():
+                sys.exit()
 
 
 def main(args):
@@ -89,7 +94,7 @@ def main(args):
         files = get_files(escFolder, args)
         for file in files:
             print(file)
-            stream_file(file, args, streamkey)
+            stream_file(file, args, streamkey, iterating=True)
     if args.file:
         file = os.path.abspath(args.file)
         stream_file(file, args, streamkey)
